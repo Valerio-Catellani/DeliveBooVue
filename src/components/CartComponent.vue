4bd5052154a73164ffc00d6e08d3bdd289fc8a33
Andrea Sgura <andre-56@hotmail.it>
2024-07-05 19:15:54 +0200

    added removeToCart and btn feature to cart

--- src/components/CartComponent.vue
+++ src/components/CartComponent.vue
@@ -7,11 +7,13 @@
             <li>{{ item.prezzo }}</li>
             <li>{{ item.qty }}</li>
         </ul>
+        <button class="btn btn-danger" :class="{ 'disabled': item.qty === 1 }" @click="removeFromCart(item)"><i class="fa-solid fa-minus">Rimuovi dal carrello</i></button>
+        <button class="btn btn-primary" @click="addToCart(item)"><i class="fa-solid fa-plus">Aggiungi al carrello</i></button>
+        
+        
     </div>
-    <button @click="addToCart(item)">Aggiungi al carrello</button>
-    <button 
-        v-if="store.cart.dishes.length > 0" 
-        @click="clearCart()">Svuota carrello
+
+    <button v-if="store.cart.dishes.length > 0" @click="clearCart()">Svuota carrello
     </button>
 
 </template>
@@ -23,7 +25,7 @@ export default {
     data() {
         return {
             store,
-            
+
         }
     },
     methods: {
@@ -46,12 +48,61 @@ export default {
             store.cart.dishes = JSON.parse(localStorage.getItem('cart'));
             console.log(store.cart.dishes);
         },
-        addToCart(item) {
-            store.cart.dishes.push({ nome: 'prova', prezzo: 10, img: 'https://picsum.photos/200/300', qty: 1 });
-            console.log(store.cart.dishes);
-            localStorage.setItem('cart', JSON.stringify(store.cart.dishes));
+        addToCart(dish) {
+            console.log('dish', dish);
+            // controllo se ci sono elementi nel carrello
+            let myCart = localStorage.getItem('cart');
+            myCart = JSON.parse(myCart);
+            console.log(myCart, 'carrello');
+
+            // se non ci sono li aggiungo
+            if (!myCart.length) {
+                console.log('nessun elemento nel carrello');
+                store.cart.dishes.push({ nome: dish.name, prezzo: dish.price, img: dish.image, qty: 1 });
+                localStorage.setItem('cart', JSON.stringify(store.cart.dishes));
+            }
+            // se ci sono 
+            else if (myCart.length) {
+                console.log(myCart);
+
+                for (let index = 0; index < myCart.length; index++) {
+                    const element = myCart[index];
+                    console.log(element, 'elemento');
+                    console.log(dish, 'nome piatto');
+
+                    // se l'elemento esiste nel carrello incremento la qty
+                    if (element.nome == dish.nome) {
+                        element.qty++;
+                        console.log('già 1');
+                        store.cart.dishes = myCart;
+                        localStorage.setItem('cart', JSON.stringify(store.cart.dishes));
+                        break;
+                    }
+
+                    // se l'elemento non esiste lo aggiungo
+                    else {
+                        store.cart.dishes.push({ nome: dish.name, prezzo: dish.price, img: dish.image, qty: 1, slug: dish.slug });
+                        localStorage.setItem('cart', JSON.stringify(store.cart.dishes));
+                        console.log(store.cart.dishes);
+                    }
+                };
+
+            }
+
         },
-        removeFromCart(item) {
+        removeFromCart(dish) {
+            let myCart = localStorage.getItem('cart');
+            myCart = JSON.parse(myCart);
+
+            for (let index = 0; index < myCart.length; index++) {
+                const element = myCart[index];
+                if (element.nome == dish.nome) {
+                        element.qty--;
+                        store.cart.dishes = myCart;
+                        localStorage.setItem('cart', JSON.stringify(store.cart.dishes));
+                        break;
+                    }
+            };
         },
         clearCart() {
             store.cart.dishes = [];
