f5a8a698d0d328e9fb96d4c1db85f2eb628bcb38
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-07-08 22:38:02 +0200

    add payment method

--- src/components/PaymentComponent.vue
+++ src/components/PaymentComponent.vue
@@ -1,32 +1,86 @@
 <template>
-    <div style="margin-top: 200px;">
-        <p>Visa:
+    <div style="margin-top: 65px;">
 
-            Numero: 4111 1111 1111 1111
-            Data di scadenza: Qualsiasi data futura
-            CVV: Qualsiasi combinazione di 3 cifre
-            MasterCard:
+        <section class="p-4 p-md-5">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-10 col-lg-8 col-xl-5">
+                    <div class="card rounded-3">
+                        <div class="card-body p-4">
+                            <div class="text-center mb-4">
+                                <h3>Seleziona il metodo di pagamento</h3>
+                            </div>
+                            <form action="">
+                                <p class="fw-bold mb-4 pb-2">Carte salvate</p>
 
-            Numero: 5555 5555 5555 4444
-            Data di scadenza: Qualsiasi data futura
-            CVV: Qualsiasi combinazione di 3 cifre
-            American Express:
+                                <div class="d-flex flex-row align-items-center mb-4 pb-1">
+                                    <img class="img-fluid"
+                                        src="https://img.icons8.com/color/48/000000/mastercard-logo.png" />
+                                    <div class="flex-fill mx-3">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" id="formControlLgXc" class="form-control form-control-lg"
+                                                value="**** **** **** 3193" />
+                                            <label class="form-label" for="formControlLgXc">Numero carta</label>
+                                        </div>
+                                    </div>
+                                    <a href="#!">Rimuovi carta</a>
+                                </div>
 
-            Numero: 3782 822463 10005
-            Data di scadenza: Qualsiasi data futura
-            CVV: Qualsiasi combinazione di 4 cifre
-            Discover:
+                                <div class="d-flex flex-row align-items-center mb-4 pb-1">
+                                    <img class="img-fluid" src="https://img.icons8.com/color/48/000000/visa.png" />
+                                    <div class="flex-fill mx-3">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" id="formControlLgXs" class="form-control form-control-lg"
+                                                value="**** **** **** 4296" />
+                                            <label class="form-label" for="formControlLgXs">Numero carta</label>
+                                        </div>
+                                    </div>
+                                    <a href="#!">Rimuovi carta</a>
+                                </div>
 
-            Numero: 6011 1111 1111 1117
-            Data di scadenza: Qualsiasi data futura
-            CVV: Qualsiasi combinazione di 3 cifre
-            JCB:
+                                <p class="fw-bold mb-4">Aggiungi nuova carta</p>
 
-            Numero: 3530 1113 3330 0000
-            Data di scadenza: Qualsiasi data futura
-            CVV: Qualsiasi combinazione di 3 cifre
-            Test Specifici con Carte di Credito</p>
-        <button @click="test">TEST</button>
+                                <div data-mdb-input-init class="form-outline mb-4">
+                                    <input type="text" id="formControlLgXsd" class="form-control form-control-lg"
+                                        placeholder="Inserisci il nome dell'intestatario" />
+                                    <label class="form-label" for="formControlLgXsd">Intestatario della carta</label>
+                                </div>
+
+                                <div class="row mb-4">
+                                    <div class="col-7">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="text" id="formControlLgXM" class="form-control form-control-lg"
+                                                value="1234 5678 1234 5678" />
+                                            <label class="form-label" for="formControlLgXM">Numero della carta</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-3">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="password" id="formControlLgExpk"
+                                                class="form-control form-control-lg" placeholder="MM/YYYY" />
+                                            <label class="form-label" for="formControlLgExpk">Data di scadenza</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-2">
+                                        <div data-mdb-input-init class="form-outline">
+                                            <input type="password" id="formControlLgcvv"
+                                                class="form-control form-control-lg" placeholder="Cvv" />
+                                            <label class="form-label" for="formControlLgcvv">Cvv</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <button data-mdb-button-init data-mdb-ripple-init
+                                    class="btn btn-success btn-lg btn-block">Aggiungi carta</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+
+
+    
         <form id="payment-form" @submit.prevent="submitPayment">
             <div id="dropin-container" style="width: 450px;"></div>
 
